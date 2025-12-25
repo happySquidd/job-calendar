8c8b866926c50691631debe335e67c2265419d49
happySquidd <rafael.safin077@gmail.com>
2025-12-25 03:15:03 -0500

    calendar rough draft

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            CalendarFlowPanel = new FlowLayoutPanel();
+            monthLabel = new Label();
+            nextBtn = new Button();
+            previousBtn = new Button();
+            SuspendLayout();
+            // 
+            // CalendarFlowPanel
+            // 
+            CalendarFlowPanel.Location = new Point(12, 92);
+            CalendarFlowPanel.Name = "CalendarFlowPanel";
+            CalendarFlowPanel.Size = new Size(395, 344);
+            CalendarFlowPanel.TabIndex = 0;
+            // 
+            // monthLabel
+            // 
+            monthLabel.AutoSize = true;
+            monthLabel.Font = new Font("Calibri", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            monthLabel.Location = new Point(177, 61);
+            monthLabel.Name = "monthLabel";
+            monthLabel.Size = new Size(61, 23);
+            monthLabel.TabIndex = 1;
+            monthLabel.Text = "month";
+            // 
+            // nextBtn
+            // 
+            nextBtn.Location = new Point(332, 63);
+            nextBtn.Name = "nextBtn";
+            nextBtn.Size = new Size(75, 23);
+            nextBtn.TabIndex = 2;
+            nextBtn.Text = "next";
+            nextBtn.UseVisualStyleBackColor = true;
+            nextBtn.Click += nextBtn_Click;
+            // 
+            // previousBtn
+            // 
+            previousBtn.Location = new Point(12, 63);
+            previousBtn.Name = "previousBtn";
+            previousBtn.Size = new Size(75, 23);
+            previousBtn.TabIndex = 3;
+            previousBtn.Text = "previous";
+            previousBtn.UseVisualStyleBackColor = true;
+            previousBtn.Click += previousBtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(419, 448);
+            Controls.Add(previousBtn);
+            Controls.Add(nextBtn);
+            Controls.Add(monthLabel);
+            Controls.Add(CalendarFlowPanel);
+            Name = "Form1";
+            Text = "Joble";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private FlowLayoutPanel CalendarFlowPanel;
+        private Label monthLabel;
+        private Button nextBtn;
+        private Button previousBtn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,43 @@ namespace job_calendar
         public Form1()
         {
             InitializeComponent();
+
+            MakeTheCalendar();
+        }
+
+        private void MakeTheCalendar()
+        {
+            monthLabel.Text = DateTime.Now.ToString("MMMM");
+            // days of the week
+            for (int i = 0; i < 7; i++)
+            {
+                Label dayName = new Label();
+                dayName.Text = Enum.GetName(typeof(DayOfWeek), i)[..3];
+                dayName.Width = 50;
+                dayName.Height = 20;
+                dayName.TextAlign = ContentAlignment.MiddleCenter;
+                CalendarFlowPanel.Controls.Add(dayName);
+            }
+            // days in month
+            int daysInMonth = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+            for (int i = 1; i <= daysInMonth; i++)
+            {
+                Button dayButton = new Button();
+                dayButton.Text = i.ToString();
+                dayButton.Width = 50;
+                dayButton.Height = 50;
+                CalendarFlowPanel.Controls.Add(dayButton);
+            }
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            CalendarFlowPanel.Controls.Clear();
+        }
+
+        private void previousBtn_Click(object sender, EventArgs e)
+        {
+            CalendarFlowPanel.Controls.Clear();
         }
     }
 }
