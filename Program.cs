2318392eba9fde24fc15a59d984d6e5f31057631
happySquidd <rafael.safin077@gmail.com>
2026-01-12 23:54:30 -0500

    connect to the database on startup

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using job_calendar.database;
+
 namespace job_calendar
 {
     internal static class Program
@@ -11,7 +13,12 @@ namespace job_calendar
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+            Database.OpenConnection();
+            
             Application.Run(new Form1());
+            
+            Database.CloseConnection();
         }
     }
 }
---
+++ database/Database.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace job_calendar.database
+{
+    internal class Database
+    {
+        private static MySqlConnection connection { get; set; }
+
+        // open connection
+        public static void OpenConnection()
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Database = "job_list",
+                UserID = "sqlUser",
+                Password = "Passw0rd!",
+                Port = 3306
+            };
+            var connectionString = builder.ConnectionString;
+
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+
+                Console.WriteLine("connection opened");
+                return;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                MessageBox.Show("Could not connect to database");
+                return;
+            }
+        }
+
+        public static void CloseConnection()
+        {
+            connection.Close();
+            Console.WriteLine("connection closed");
+        }
+    }
+}
